b22725b0006fa4483f388eaffea1556923cf20e5
Ariel Gribov <arielgribov@MacBookPro.lan>
2025-06-10 22:46:20 +0300

    update api call

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import requests
+import urllib.parse
+
 
 app = FastAPI()
 
@@ -11,11 +13,25 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+def geocode_city(city: str):
+    url = f"https://nominatim.openstreetmap.org/search?q={urllib.parse.quote(city)}&format=json&limit=1"
+    headers = {"User-Agent": "WeatherApp/1.0"}
+    response = requests.get(url, headers=headers)
+    data = response.json()
+
+    if not data:
+        raise Exception("Could not geocode city")
+
+    lat = data[0]["lat"]
+    lon = data[0]["lon"]
+    return lat, lon
 
 @app.get("/")
 async def get_weather(city):
     try:
-        WEATHER_API_URL = f"https://wttr.in/{city}?format=j1"
+       # WEATHER_API_URL = f"https://wttr.in/{city}?format=j1"
+        lat, lon = geocode_city(city)
+        WEATHER_API_URL = f"https://wttr.in/~{lat},{lon}?format=j1"
 
         response = requests.get(WEATHER_API_URL)
         data = response.json()
