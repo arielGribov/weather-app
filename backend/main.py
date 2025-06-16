2fd13774b6ae44f8ea0f5b00431c7fe1032faa52
Ariel Gribov <arielgribov@MacBookPro.lan>
2025-06-16 22:27:28 +0300

    first updates

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import requests
+## import requests
 import urllib.parse
-
+import httpx
+import logging
 
 app = FastAPI()
 
@@ -10,11 +11,13 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     # allow_origins=["https://yourfrontend.com"] lets change this to a specific domain - why? give me a reason please
+    ## since i do not have a dns record, can i do it ? at the moment the front svc pub ip changes everytime
+    # allow_origins=["*/elb.amazonaws.com"], ##?
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-def geocode_city(city: str):
+async def geocode_city(city: str):
     #You call requests.get() inside a helper (geocode_city) but invoke it from an async route.
     # This leads to the same blocking issue as above
     #
@@ -26,7 +29,9 @@ def geocode_city(city: str):
 
     url = f"https://nominatim.openstreetmap.org/search?q={urllib.parse.quote(city)}&format=json&limit=1"
     headers = {"User-Agent": "WeatherApp/1.0"}
-    response = requests.get(url, headers=headers)
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, headers=headers)
+    ## response = requests.get(url, headers=headers)
     data = response.json()
 
     if not data:
@@ -39,11 +44,11 @@ def geocode_city(city: str):
 @app.get("/")
 async def get_weather(city):
     try:
-       # WEATHER_API_URL = f"https://wttr.in/{city}?format=j1"
-        lat, lon = geocode_city(city)
+        lat, lon = await geocode_city(city)
         WEATHER_API_URL = f"https://wttr.in/~{lat},{lon}?format=j1"
 
-        response = requests.get(WEATHER_API_URL)
+        ## response = requests.get(WEATHER_API_URL)
+        
         # lets change this to async compatibility
         # FastAPI is built for high performance via asynchronous I/O.
         # Using requests blocks the event loop, reducing throughput and scalability.
@@ -54,8 +59,11 @@ async def get_weather(city):
         #   response = await client.get(WEATHER_API_URL)
         #   response.raise_for_status()
         #   data = response.json()
-
-        data = response.json()
+        async with httpx.AsyncClient() as client:
+          response = await client.get(WEATHER_API_URL)
+          response.raise_for_status()
+          data = response.json()
+        ## data = response.json()
 
         current = data["current_condition"][0]
         return {
@@ -65,7 +73,12 @@ async def get_weather(city):
             "humidity": current["humidity"] + "%",
             "wind_speed": current["windspeedKmph"] + " km/h"
         }
-    except Exception as e:
+  
+    except Exception as e:  
+        logger = logging.getLogger("weather_app")
+        logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+        logger.exception("Error fetching weather data") 
+        return {"error": str(e)}
     #Improve Error Handling and Observability
 
     # import logging
@@ -76,4 +89,4 @@ async def get_weather(city):
     # except Exception as e:
     #     logger.exception("Error fetching weather data")
     #     return {"error": "Internal server error"}
-        return {"error": str(e)}
+       
