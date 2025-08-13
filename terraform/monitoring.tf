f615c7afa9be11283a340949e92936b11c000b4b
arielGribov <argr96@gmail.com>
2025-08-13 19:15:34 +0300

    prom+graf

--- terraform/monitoring.tf
+++ terraform/monitoring.tf
@@ -24,4 +24,24 @@
 
 
 
-# dddfd
+
+
+
+
+
+
+# resource "kubernetes_namespace" "kube-namespace" {
+#   depends_on = [module.eks]
+#   metadata {
+#     name = "monitoring"
+#   }
+# }
+# resource "helm_release" "prometheus" {
+#   depends_on       = [kubernetes_namespace.kube-namespace]
+#   name             = "kube-prometheus-stack"
+#   repository       = "https://prometheus-community.github.io/helm-charts"
+#   chart            = "kube-prometheus-stack"
+#   namespace        = kubernetes_namespace.kube-namespace.metadata[0].name
+#   create_namespace = false
+#   values           = [file("kube-prom-values.yaml")]
+# }
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -41,13 +41,6 @@ provider "kubectl" {
   load_config_file       = false
 }
 
-# provider "helm" {
-#   kubernetes = {
-#     host                   = data.aws_eks_cluster.eks_id.endpoint
-#     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_id.certificate_authority[0].data)
-#     token                  = data.aws_eks_cluster_auth.eks_auth.token
-#   }
-# }
 provider "helm" {
   kubernetes = {
     host                   = module.eks.cluster_endpoint
