ad37a73178322fd17fe845054e78ff823e4d2eba
arielGribov <argr96@gmail.com>
2025-08-17 19:55:00 +0300

    prom+graf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -47,6 +47,7 @@ provider "helm" {
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_id.certificate_authority[0].data)
   }
 }
+
 # provider "helm" {
 #   kubernetes = {
 #     host                   = module.eks.cluster_endpoint
