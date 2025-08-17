e69a3286a74ab7367858b4c6fa9781db787f4af1
arielGribov <argr96@gmail.com>
2025-08-17 18:45:48 +0300

    prom+graf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -47,3 +47,16 @@ provider "helm" {
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_id.certificate_authority[0].data)
   }
 }
+# provider "helm" {
+#   kubernetes = {
+#     host                   = module.eks.cluster_endpoint
+#     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+#     exec = {
+#       api_version = "client.authentication.k8s.io/v1beta1"
+#       command     = "aws"
+#       # This requires the awscli to be installed locally where Terraform is executed
+#       args = ["eks", "get-token", "--cluster-name", module.eks.cluster_id]
+#     }
+#   }
+# }
