1407c08bfb483919295ea4ac8511b8b2ffc7af4c
arielGribov <argr96@gmail.com>
2025-08-19 21:32:19 +0300

    prom+graf

--- terraform/monitoring.tf
+++ terraform/monitoring.tf
@@ -1,12 +1,12 @@
 
 data "aws_eks_cluster" "eks_id" {
   depends_on = [module.eks]
-  name       = module.eks.cluster_name
+  name       = var.eks_name
 }
 
 data "aws_eks_cluster_auth" "eks_auth" {
   depends_on = [module.eks]
-  name       = module.eks.cluster_name
+  name       = var.eks_name
 }
 
 
